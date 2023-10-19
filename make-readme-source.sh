447bd1562b1dfd0dc97b12a1f9b8069c3571ebf0
Papp Tamás <tpapp@npsh.hu>
2023-10-20 00:05:16 +0200

    remove sub toc

--- make-readme-source.sh
+++ make-readme-source.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e ":source-highlighter: rouge\n\n= TIL\n\nToday in learned.\n\n:toc:\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
+echo -e ":source-highlighter: rouge\n:toc:\n:toc-placement!:\n\n= TIL\n\nToday in learned.\n\ntoc::[]\n\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
