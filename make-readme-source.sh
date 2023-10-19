39b2823349e7080a01c1299308a24f9dc02c8357
Papp Tamás <tpapp@npsh.hu>
2023-10-19 23:45:14 +0200

    run

--- make-readme-source.sh
+++ make-readme-source.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "\n= TIL\n:source-highlighter: rouge\n\nToday in learned.\n\n:toc:\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
+echo -e "\n= TIL\n:source-highlighter: rouge\n\nToday in learned.\n:toc:\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
