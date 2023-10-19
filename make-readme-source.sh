c192ae1e6fb314d4f48ac319069848eb0950f4e7
Papp Tamás <tpapp@npsh.hu>
2023-10-20 00:22:34 +0200

    add toc

--- make-readme-source.sh
+++ make-readme-source.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e ":source-highlighter: rouge\n:toc:\n\n\nToday in learned.\n\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
+echo -e ":source-highlighter: rouge\n\nToday in learned.\n\n" > readme-source.adoc; for i in $(find . -name  '*.adoc' | grep -v README.adoc | cut -c3-) ; do NAME=${i%%.adoc}  ; echo $NAME ; done |sort| awk '{print "include::"$1".adoc["$i"]\n"}' >> readme-source.adoc
